8fda25c062dafbb703dd095770163b43f705cd1e
JonasASchmidt <jonas.a.schmidt@gmail.com>
2020-12-12 14:16:13 +0100

    Optimized bg image and added click on last sentence to go back to the beginning.

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function (event) { // Fire when DOM is loaded
+document.addEventListener("DOMContentLoaded", function (event) { // Execute when DOM is loaded
     //event.preventDefault(); // ?
 
     // Define number of sections by array of sentences
@@ -38,28 +38,25 @@ document.addEventListener("DOMContentLoaded", function (event) { // Fire when DO
     let body = document.querySelector("body"); // Define variable body as pointer to <body> element.
     let scrollPosition = 0; // Set initial scroll position
 
-    // Generate image sections with sentences
-    sentences.forEach((sentence) => {
+    // +++ Generate image sections with sentences
+    sentences.forEach((sentence, idx) => {
+
         let section = document.createElement("div"); // Create a DIV node
-
         section.classList.add("section-image"); // Add class to div
-
         let randomKeywords = ""; // Create variable for random image keywords
 
         for (let i = 0; i < 3; i++) {
             let randomKeyword = keywords[Math.floor(Math.random() * keywords.length)]; // Get random keyword from array
-            if (i !== 0) {
-                randomKeywords += "," + randomKeyword; // Add keyword to keywords variable
-            } else {
-                randomKeywords = randomKeyword; // Add keyword to keywords variable
-            }
+            if (i === 0) { // If not adding the first keyword
+                randomKeywords = randomKeyword; // Reassign keyword (1st) to keywords variable
 
+            } else {
+                randomKeywords += "," + randomKeyword; // Add , + keyword to keywords variable
+            }
         }
 
-        // console.log(randomKeywords);
-
-        // Build up HTML structure and add it to body
-        section.style = `background-image:url('https://source.unsplash.com/1600x900/?${randomKeywords}')`; // Add background image to div via inline style with url
+        // +++ Build up HTML structure and add it to body
+        section.style = `background-image: url('https://source.unsplash.com/1600x900/?${randomKeywords}')`; // Add background image to div via inline style with url
         let textnode = document.createElement("span"); // Create span for the sentence text
         textnode.classList.add("sentence"); // Add class to span
         let text = document.createTextNode(sentence); // Create a text node with the sentence
@@ -67,37 +64,47 @@ document.addEventListener("DOMContentLoaded", function (event) { // Fire when DO
         section.appendChild(textnode); // Append span as child of div
         body.appendChild(section); // Append div as child of body
 
-        // Add event listener to each sentence
-        textnode.addEventListener("click", function (event) {
-            scrollPosition += window.innerHeight; // Set viewport height as scroll position
+        // +++ Add event listener to each sentence
+        // textnode.addEventListener("click", function (event) {
+        //     scrollPosition += window.innerHeight; // Set viewport height as scroll position
 
-            var body = document.body,
-                html = document.documentElement;
+        //     var body = document.body,
+        //         html = document.documentElement;
 
-            var height = Math.max(
-                body.scrollHeight,
-                body.offsetHeight,
-                html.clientHeight,
-                html.scrollHeight,
-                html.offsetHeight
-            );
+        //     var height = Math.max(
+        //         body.scrollHeight,
+        //         body.offsetHeight,
+        //         html.clientHeight,
+        //         html.scrollHeight,
+        //         html.offsetHeight
+        //     );
 
-            if (scrollPosition >= height) {
-                //document.body.scrollTop = 0;
-                //document.documentElement.scrollTop = 0;
+        //     if (scrollPosition >= height) {
+
+        //         window.scrollTo({
+        //             top: 0,
+        //             behavior: "smooth"
+        //         });
+        //         scrollPosition = 0;
+        //     } else {
+        //         window.scrollTo({
+        //             top: scrollPosition,
+        //             behavior: "smooth"
+        //         });
+        //     }
+
+        // });
+
+        // +++ Add event listener to last sentence
+        if ( idx === (sentences.length - 1) ) {
+            textnode.addEventListener("click", function (event) {
                 window.scrollTo({
                     top: 0,
                     behavior: "smooth"
                 });
                 scrollPosition = 0;
-            } else {
-                window.scrollTo({
-                    top: scrollPosition,
-                    behavior: "smooth"
-                });
-            }
-            
-        });
+            });
+        }
     });
     //console.log("DOM fully loaded and parsed");
 });
